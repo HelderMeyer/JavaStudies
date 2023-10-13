97546da0994d963a1ff458f01cd3183642aefe8b
Helder Meyer <helder.meyer3010@gmail.com>
2023-10-13 18:11:41 -0300

    Second commit

--- EstudosOO/src/roteiro1/parte1/Programa01.java
+++ EstudosOO/src/roteiro1/parte1/Programa01.java
@@ -3,8 +3,7 @@ package roteiro1.parte1;
 public class Programa01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World!");
 	}
 
 }
